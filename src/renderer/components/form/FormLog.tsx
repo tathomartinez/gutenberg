e6a08a2011231250f86cf224575847843330954a
Joan Martinez Moreno <sebastian.martinez@sii-group.co>
2023-12-06 21:56:38 -0500

    Now get Select devices

--- src/renderer/components/form/FormLog.tsx
+++ src/renderer/components/form/FormLog.tsx
@@ -1,6 +1,15 @@
-import { TextField, Button, Stack } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Stack,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
-import Adb from "@devicefarmer/adbkit";
+import Adb, { Device } from "@devicefarmer/adbkit";
 
 import SearchIcon from "@mui/icons-material/Search";
 import { EventEmitter } from "stream";
@@ -9,13 +18,20 @@ let content = "";
 
 export default function FormLogs({ log }: { log(content: string): void }) {
   const [tag, setTag] = useState("Neil");
+  const [device, setDevice] = useState("");
+  const [deviceList, setDeviceList] = useState(["Oliver Hansen"]); // Ahora names es un estado
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setDevice(event.target.value as string);
+  };
 
   useEffect(() => {
     const client = Adb.createClient();
+
     const subscribeToLogcat = async () => {
       try {
-        const device = await client.getDevice("emulator-5554");
-        device.openLogcat({ clear: true }).then((logcat: EventEmitter) => {
+        const deviceChose = await client.getDevice(device);
+        deviceChose.openLogcat({ clear: true }).then((logcat: EventEmitter) => {
           logcat.on("entry", (entry) => {
             if (entry.tag === tag) {
               log((content += entry.message + "\n"));
@@ -28,25 +44,62 @@ export default function FormLogs({ log }: { log(content: string): void }) {
     };
     log("");
     subscribeToLogcat();
-  }, [tag]);
+  }, [tag, device]);
 
   const handleTagChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // content = "";
-    // logcat?.removeAllListeners;
     setTag(event.target.value); // Actualiza el estado con la nueva etiqueta
+  };
+
+  const listarDevices = () => {
+    const client = Adb.createClient();
+    const listClient = async () => {
+      try {
+        client.listDevices().then((devices: Device[]) => {
+          const deviceNames = devices.map((device) => device.id); // Crea un array con los nombres de los dispositivos
+          setDeviceList(deviceNames); // Actualiza el estado names
+        });
+      } catch (err) {
+        console.error("Something went wrong:", err);
+      }
+    };
+    listClient();
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     content = "";
-    log("")
-    // logcat?.removeAllListeners;
+    log("");
     setTag(tag);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <Stack direction="column" spacing={2}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Devices</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={device}
+            label="Device"
+            onChange={handleChange}
+          >
+            {deviceList.map((device) => {
+              return (
+                <MenuItem key={device} value={device}>
+                  {device}
+                </MenuItem>
+              );
+            })}
+          </Select>
+          <Button
+            variant="outlined"
+            onClick={listarDevices}
+            startIcon={<SearchIcon />}
+          >
+            Listar devices
+          </Button>
+        </FormControl>
         <TextField
           id="outlined-basic"
           label="Filtro"
